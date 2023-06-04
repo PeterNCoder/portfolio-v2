f55ab910c63b6b82ffe89af8d30dacc99a6c84b1
Peter Nguyen <peternguyenwebdev@gmail.com>
2023-06-04 16:26:40 -0700

    Update next.config.js and move fonts to public

--- portfolio-nextjs/next.config.js
+++ portfolio-nextjs/next.config.js
@@ -1,10 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: 'export',
-    assetPrefix: 'https://www.petervnguyen.com/',
-    images: {
-        unoptimized: true
-    }
 }
 
 module.exports = nextConfig
